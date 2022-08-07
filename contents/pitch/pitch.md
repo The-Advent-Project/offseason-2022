92380d99c812e2624deb6777ec31278cee092bc8
Austin Heller <me@aheller.com>
2022-08-07 07:11:12 +0000

    Basic pitch form

--- contents/pitch/pitch.md
+++ contents/pitch/pitch.md
@@ -1,5 +1,45 @@
-title: Submit an Idea
+title: Pitch a Piece
+id: pitch
 
 ~~
 
-Pitch page
+#### Every winter, The Advent Project calendar reaches hundreds of people who want to benefit a good cause and open something nice together.
+
+Last year, we raised $X for Grassroots Global Justice Alliance, which was made possible by the artists who donated 24 pieces of brand-new, bite-sized multimedia art. And this year, you could be one of those artists!
+
+Questions? We have answers:
+
+### What makes an Advent Project piece?
+
+An ideal Advent Project piece is
+
+* any media or tone,
+* able to be consumed in under five minutes,
+* brand-new or in the very early stages of development, and
+* loosely tied to winter or a holiday that takes place in the winter.
+
+We particularly love work that is cross-media, collaborative, and reflective of diverse backgrounds and holiday traditions. (We borrow the term "advent" from the Christmas tradition, but our project is not bound to any particular religion or holiday.) Art that has been published elsewhere (including elsewhere on the internet) is not eligible.
+
+If this sounds like a good home for the tiny piece of weird and/or wonderful art that's been living in the back of your brain, please tell us about it below.
+
+### Where do the proceeds of the calendar go?
+
+All proceeds from sales of the calendar go to the charity that is chosen in collaboration with our artists. If you choose to participate, thank you in advance for donating your time and your art.
+
+### Who creates the piece?
+
+You do! But if your pitch is selected, you'll be paired with an editor who can answer creative and technical questions, and help you get your piece over the finish line.
+
+### If my pitch is accepted, do I maintain ownership of my final work?
+
+**Yes**. You will always retain rights to work you make for the calendar. (And any pitch you submit also remains yours, even if you don't make it with us.)
+
+### I have a rough idea, but it's not complete. Should I still pitch?
+
+Don't worry about having a whole project mapped out yet—a short premise is all you need to pitch. Some of our favorite pieces started as nothing more than a couple sentences. For example: *A holiday-themed comedy meditation track. Maybe with festive sound effects sprinkled in?*
+
+But it also helps if you send us your thoughts, dreams, work samples, mood boards, whatever will help us understand who you are and what you're interested in making. We'll ask questions as needed to help you draw out your idea and see if it's a good fit for this year's Advent Project.
+
+### I'm having trouble filling out this form.
+
+First, try opening the form [in a new window](https://8km48pp3tz9.typeform.com/to/Y9zBC6oz). If that's not working, email us at [info@adventproject.org](mailto:info@adventproject.org), let us know you're interested in submitting, and we'll get things sorted.
--- contents/scripts/index.js
+++ contents/scripts/index.js
@@ -1,1 +1,92 @@
-console.log('hey guys')
+/* global fitvids */
+import './fitvids.js';
+
+/* Fitvids */
+fitvids()
+
+/* Open external links in a new tab */
+document.querySelectorAll('a[href*="http"]').forEach(link => {
+  link.setAttribute('target', '_blank')
+})
+
+/* Expandables */
+// Find auto-expand container
+const autoExpands = document.querySelectorAll('*[data-auto-expand="yes"]');
+
+function insertAfter(newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
+
+function createExpandContainer(expandId = '') {
+  const container = document.createElement('div');
+  container.setAttribute('data-expandable', expandId)
+  container.classList.add('expandable');
+  return container;
+}
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+autoExpands.forEach( container => {
+  let expandChildren = [], lastHeading = null, i = 0;
+  const immediateChildren = container.querySelectorAll(':scope > *');
+  immediateChildren.forEach( child => {
+    if(child.tagName === "H3") {
+      if( ! child.hasAttribute('id') ) {
+       child.setAttribute('id', uuidv4() );
+      }
+      // child.setAttribute( 'data-expand', child.getAttribute('id') );
+      if( child !== lastHeading ) {
+        if( expandChildren.length > 0 ) {
+          // Create expand container, add children to it
+          const expandContainer = createExpandContainer( lastHeading.getAttribute('id') );
+          expandChildren.forEach( child => { expandContainer.appendChild(child); } )
+          insertAfter( expandContainer, lastHeading );          
+          expandChildren.length = 0;
+        }
+        lastHeading = child;
+      }
+    } else { // not an h3
+      if( lastHeading !== null ) {
+        expandChildren.push(child);
+      }
+    }
+    if(i + 1 === immediateChildren.length && expandChildren.length > 0) {
+      const expandContainer = createExpandContainer( lastHeading.getAttribute('id') );
+      expandChildren.forEach( child => { expandContainer.appendChild(child); } )
+      insertAfter( expandContainer, lastHeading );          
+    }
+    i++;
+  } );
+} )
+
+const expandables = document.querySelectorAll('*[data-expandable]');
+expandables.forEach( expandable => {
+  const expandName = expandable.getAttribute('data-expandable');
+  const expandToggle = document.querySelector('#' + expandName);
+  expandToggle.setAttribute('data-expand-toggle', expandName);
+  expandToggle.setAttribute('data-expand-state', 'visible');
+  if(! expandable.hasAttribute('data-default-expanded') ) {
+	expandable.classList.add('hidden');
+	expandToggle.setAttribute('data-state', 'hidden');
+  }
+  expandToggle.addEventListener('click', (e) => {
+	 e.preventDefault();
+	 doExpandable(expandable, expandToggle)
+  } )
+} )
+
+function doExpandable(expandable, expandToggle) {
+  if( expandable.classList.contains('hidden') ) {
+	 expandable.classList.remove('hidden');
+	 expandToggle.setAttribute('data-state', 'visible');
+  }
+  else {
+	 expandable.classList.add('hidden');
+	 expandToggle.setAttribute('data-state', 'hidden');
+  }
+}
